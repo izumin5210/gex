90e27e6252c509c8a54f6022ecddd59b50cf759d
izumin5210 <m@izum.in>
2018-09-28 10:37:04 +0900

    Inject $PWD/bin to $PATH on executing commands

--- pkg/command/executor.go
+++ pkg/command/executor.go
@@ -3,7 +3,10 @@ package command
 import (
 	"context"
 	"io"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 // Executor is an interface for executing commands.
@@ -12,17 +15,29 @@ type Executor interface {
 }
 
 // NewExecutor creates a new Executor instance.
-func NewExecutor(outW, errW io.Writer, inR io.Reader) Executor {
+func NewExecutor(outW, errW io.Writer, inR io.Reader, cwd string) Executor {
+	env := os.Environ()
+	for _, e := range env {
+		kv := strings.SplitN(e, "=", 2)
+		if kv[0] == "PATH" {
+			kv[1] = filepath.Join(cwd, "bin") + string(os.PathListSeparator) + kv[1]
+		}
+		env = append(env, strings.Join(kv, "="))
+	}
 	return &executorImpl{
 		outW: outW,
 		errW: errW,
 		inR:  inR,
+		cwd:  cwd,
+		env:  env,
 	}
 }
 
 type executorImpl struct {
 	outW, errW io.Writer
 	inR        io.Reader
+	cwd        string
+	env        []string
 }
 
 func (e *executorImpl) Exec(ctx context.Context, name string, args ...string) error {
@@ -30,5 +45,7 @@ func (e *executorImpl) Exec(ctx context.Context, name string, args ...string) er
 	cmd.Stdout = e.outW
 	cmd.Stderr = e.errW
 	cmd.Stdin = e.inR
+	cmd.Dir = e.cwd
+	cmd.Env = e.env
 	return cmd.Run()
 }
