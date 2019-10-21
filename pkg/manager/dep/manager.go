a4e1a321581573eac8925a9276628cdc4426946d
izumin5210 <m@izum.in>
2019-10-21 09:18:07 +0900

    Reduce redundant command execution

--- pkg/manager/dep/manager.go
+++ pkg/manager/dep/manager.go
@@ -2,8 +2,6 @@ package dep
 
 import (
 	"context"
-	"encoding/json"
-	"path"
 	"path/filepath"
 	"strings"
 
@@ -25,26 +23,6 @@ type managerImpl struct {
 	executor   manager.Executor
 	rootDir    string
 	workingDir string
-}
-
-func (m *managerImpl) Add(ctx context.Context, pkgs []string, verbose bool) error {
-	var err error
-	pkgs, err = m.pickNewPackages(ctx, pkgs)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	if len(pkgs) == 0 {
-		return nil
-	}
-
-	args := []string{"ensure"}
-	if verbose {
-		args = append(args, "-v")
-	}
-	args = append(args, "-add")
-	args = append(args, pkgs...)
-	return errors.WithStack(m.executor.Exec(ctx, "dep", args...))
 }
 
 func (m *managerImpl) Build(ctx context.Context, binPath, pkg string, verbose bool) error {
@@ -70,51 +48,4 @@ func (m *managerImpl) Sync(ctx context.Context, verbose bool) error {
 		args = append(args, "-v")
 	}
 	return errors.WithStack(m.executor.Exec(ctx, "dep", args...))
-}
-
-func (m *managerImpl) pickNewPackages(ctx context.Context, pkgs []string) ([]string, error) {
-	pkgSet, err := m.getExistingPackageSet(ctx)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	if len(pkgSet) == 0 {
-		return pkgs, nil
-	}
-
-	result := make([]string, 0, len(pkgs))
-
-	for _, pkg := range pkgs {
-		var skipped bool
-		for pkg := pkg; pkg != "."; pkg = path.Dir(pkg) {
-			if _, ok := pkgSet[pkg]; ok {
-				skipped = true
-				break
-			}
-		}
-		if !skipped {
-			result = append(result, pkg)
-		}
-	}
-
-	return result, nil
-}
-
-func (m *managerImpl) getExistingPackageSet(ctx context.Context) (map[string]struct{}, error) {
-	out, err := m.executor.Output(ctx, "dep", "status", "-json")
-	if err != nil {
-		return make(map[string]struct{}), nil
-	}
-	pkgs := []struct{ ProjectRoot string }{}
-	err = json.Unmarshal(out, &pkgs)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	pkgRoots := make(map[string]struct{}, len(pkgs))
-	for _, pkg := range pkgs {
-		pkgRoots[pkg.ProjectRoot] = struct{}{}
-	}
-
-	return pkgRoots, nil
 }
