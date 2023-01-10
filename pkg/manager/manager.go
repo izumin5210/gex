01ea833dfea1989154dbd1c2217cc1bd0f1ad875
Masaki Hara <ackie.h.gmai@gmail.com>
2023-01-10 17:57:37 +0900

    Allow gradual migration to go run

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -5,5 +5,6 @@ import "context"
 type Interface interface {
 	Add(ctx context.Context, pkgs []string, verbose bool) error
 	Build(ctx context.Context, binPath, pkg string, verbose bool) error
+	RunInPlace(ctx context.Context, pkg string, verbose bool, args ...string) error
 	Sync(ctx context.Context, verbose bool) error
 }
--- pkg/tool/manifest.go
+++ pkg/tool/manifest.go
@@ -2,26 +2,37 @@ package tool
 
 import (
 	"sort"
+	"strings"
 
 	"github.com/izumin5210/gex/pkg/manager"
 )
 
 // Manifest contains tool list
 type Manifest struct {
-	toolMap     map[string]Tool
-	managerType manager.Type
+	toolMap          map[string]Tool
+	managerType      manager.Type
+	defaultBuildMode BuildMode
 }
 
 // NewManifest creates a new Manifest instance.
-func NewManifest(tools []Tool, mType manager.Type) *Manifest {
+func NewManifest(tools []Tool, mType manager.Type, options ...ManifestOption) *Manifest {
 	toolMap := make(map[string]Tool, len(tools))
 	for _, t := range tools {
 		toolMap[t.Name()] = t
 	}
-	return &Manifest{toolMap: toolMap, managerType: mType}
+	m := &Manifest{toolMap: toolMap, managerType: mType}
+	for _, option := range options {
+		option(m)
+	}
+	if m.defaultBuildMode == BuildModeUnknown {
+		m.defaultBuildMode = BuildModeBin
+	}
+	return m
 }
 
 func (m *Manifest) ManagerType() manager.Type { return m.managerType }
+
+func (m *Manifest) DefaultBuildMode() BuildMode { return m.defaultBuildMode }
 
 // AddTool adds a new tool to the manifest.
 func (m *Manifest) AddTool(tool Tool) {
@@ -37,18 +48,25 @@ func (m *Manifest) FindTool(name string) (t Tool, ok bool) {
 // Tools returns a tool list.
 func (m *Manifest) Tools() []Tool {
 	n := len(m.toolMap)
-	s := make([]string, 0, n)
+	ts := make([]Tool, 0, n)
 	for _, t := range m.toolMap {
-		s = append(s, string(t))
+		ts = append(ts, t)
 	}
-	sort.StringSlice(s).Sort()
-	ts := make([]Tool, n, n)
-	for i, t := range s {
-		ts[i] = Tool(t)
-	}
+	sort.Slice(ts, func(i int, j int) bool {
+		return strings.Compare(ts[i].ImportPath, ts[j].ImportPath) < 0
+	})
 	return ts
 }
 
 func (m *Manifest) addTool(t Tool) {
 	m.toolMap[t.Name()] = t
+}
+
+type ManifestOption func(m *Manifest)
+
+// WithDefaultBuildMode configures default build mode.
+func WithDefaultBuildMode(buildMode BuildMode) ManifestOption {
+	return func(m *Manifest) {
+		m.defaultBuildMode = buildMode
+	}
 }
